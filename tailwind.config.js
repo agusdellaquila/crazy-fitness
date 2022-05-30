97232d64afed179b8ebece525d0a026be03ba068
agusdellaquila <agusdellaquila72@gmail.com>
2022-05-30 02:55:17 -0300

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#1A191F',
+      'secondary': '#ffed4a',
+      'danger': '#e3342f',
+    })
+  },
+  plugins: [],
+}
